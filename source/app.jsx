6756c8e6e3873c3cbe0bd82cc434e8e03308907d
johnbrowe <john.andreassen83@gmail.com>
2016-11-09 21:15:29 +0100

    Added new learnings

---
+++ source/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+    render
+}
+
+from 'react-dom';
+const App=React.createClass( {
+    render() {
+        return ( <section> <h1>My scaffold</h1> <p>Hello world</p> </section>);
+    }
+}
+
+);
+render ( <App />, document.getElementById('container'));
